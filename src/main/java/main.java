af6af0d1af13b793a206c49ca14531f2af41a2e7
ikm216 <ikmclassof22@gmail.com>
2024-11-30 22:07:48 -0500

    class:finished to main method to be able to run the program

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,1 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+        InternalNode rootNode = new InternalNode(0, 0, 100, 100);
 
+        System.out.println("Inserting Rectangles:");
+        Rect rect1 = new Rect(10, 10, 5, 5);
+        Rect rect2 = new Rect(15, 15, 3, 3);
+        Rect rect3 = new Rect(5, 5, 2, 2);
+        Rect rect4 = new Rect(20, 20, 4, 4);
+
+        rootNode.insertRect(rect1);
+        rootNode.insertRect(rect2);
+        rootNode.insertRect(rect3);
+        rootNode.insertRect(rect4);
+
+        System.out.println("\nTree Structure After Insertion:");
+        rootNode.dump(0);
+
+        System.out.println("\nFinding Rectangle at (15, 15):");
+        Rect foundRect = rootNode.findRect(15, 15);
+        if (foundRect != null) {
+            System.out.println("Found rectangle: Length = " + foundRect.getLen() + ", Width = " + foundRect.getWid());
+        } else {
+            System.out.println("Rectangle not found.");
+        }
+
+        System.out.println("\nUpdating Rectangle at (10, 10):");
+        rootNode.updateRect(rect1, 10, 10); 
+        rootNode.dump(0);
+
+        System.out.println("\nDeleting Rectangle at (20, 20):");
+        boolean isDeleted = rootNode.deleteRect(rect4);
+        if (isDeleted) {
+            System.out.println("Rectangle deleted successfully.");
+        } else {
+            System.out.println("Rectangle not found.");
+        }
+
+        System.out.println("\nTree Structure After Deletion:");
+        rootNode.dump(0);
+    }
+}
